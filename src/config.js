de69ea625d9968b81326243b3ba09bcc5834a6ba
Bayronalpizar99 <bayalpiizar777@gmail.com>
2025-11-05 13:24:18 -0600

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+//const config = {
+    //WEBSOCKET_URL:
+
+       // 'https://simple-video-app-maovares.azurewebsites.net'
+//};
+
+//export default config;
+const config = {
+    // URL anterior (producción): 'https'://simple-video-app-maovares.azurewebsites.net'
+    WEBSOCKET_URL: 'ws://localhost:8000' // Usa 'ws' y el puerto 8000
+};
+export default config;
